2daec8a46e4952dab1f8003a93f63d9e72067e99
DonLudo <eftl.rennes@eftl.com>
2019-05-07 14:24:15 +0200

    Integration test

--- src/test/java/fr/eftl/agence/voyages/test/TestCalculPrixBillet.java
+++ src/test/java/fr/eftl/agence/voyages/test/TestCalculPrixBillet.java
@@ -24,4 +24,48 @@ public class TestCalculPrixBillet {
 		
 	}
 
+	@Test
+	public void prixBilletClasseBus() {
+		
+		CalculPrixBillet prixBillet = new CalculPrixBillet();
+		//CalculPrixUnitaire calculPrixUnitaire = new CalculPrixUnitaire();
+		CalculPrixUnitaire calculPrixUnitaire = Mockito.mock(CalculPrixUnitaire.class);
+		Mockito.when(calculPrixUnitaire.generer()).thenReturn(200);
+		
+		prixBillet.setCalculPrixUnitaire(calculPrixUnitaire);
+		
+		Integer prixObtenu = prixBillet.calculer("Paris", "Londres", "buisness");
+		Assert.assertEquals(700, prixObtenu,0);
+		
+	}
+	
+	@Test
+	public void prixBilletClassePremiere() {
+		
+		CalculPrixBillet prixBillet = new CalculPrixBillet();
+		//CalculPrixUnitaire calculPrixUnitaire = new CalculPrixUnitaire();
+		CalculPrixUnitaire calculPrixUnitaire = Mockito.mock(CalculPrixUnitaire.class);
+		Mockito.when(calculPrixUnitaire.generer()).thenReturn(200);
+		
+		prixBillet.setCalculPrixUnitaire(calculPrixUnitaire);
+		
+		Integer prixObtenu = prixBillet.calculer("Paris", "Londres", "premiere");
+		Assert.assertEquals(400, prixObtenu,0);
+		
+	}
+	
+	@Test
+	public void prixBilletClasseDefault() {
+		
+		CalculPrixBillet prixBillet = new CalculPrixBillet();
+		//CalculPrixUnitaire calculPrixUnitaire = new CalculPrixUnitaire();
+		CalculPrixUnitaire calculPrixUnitaire = Mockito.mock(CalculPrixUnitaire.class);
+		Mockito.when(calculPrixUnitaire.generer()).thenReturn(200);
+		
+		prixBillet.setCalculPrixUnitaire(calculPrixUnitaire);
+		
+		Integer prixObtenu = prixBillet.calculer("Paris", "Londres", "");
+		Assert.assertEquals(200, prixObtenu,0);
+		
+	}
 }
