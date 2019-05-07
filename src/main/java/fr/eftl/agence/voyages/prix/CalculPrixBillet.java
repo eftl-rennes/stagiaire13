36b91377a5e67aedfcffd406c28c968151088111
DonLudo <eftl.rennes@eftl.com>
2019-05-07 10:51:05 +0200

    integration test

---
+++ src/main/java/fr/eftl/agence/voyages/prix/CalculPrixBillet.java
@@ -0,0 +1,50 @@
+package fr.eftl.agence.voyages.prix;
+
+public class CalculPrixBillet {
+
+	private CalculPrixUnitaire calculPrixUnitaire;
+	private Integer prixUnitaire; 
+	private String classe;
+	
+	public Integer getPrixUnitaire() {
+		return prixUnitaire;
+	}
+	public void setPrixUnitaire(Integer prixUnitaire) {
+		this.prixUnitaire = prixUnitaire;
+	}
+	public String getClasse() {
+		return classe;
+	}
+	public void setClasse(String classe) {
+		this.classe = classe;
+	}
+	
+	public Integer calculer(String depart, String arrivee, String classe) {
+		
+		calculPrixUnitaire.setDepart(depart);
+		calculPrixUnitaire.setArrivee(arrivee);
+		Integer varPrixUnitaire = calculPrixUnitaire.generer();
+		
+		switch(classe) {
+		case "eco":
+			return varPrixUnitaire;
+			
+		case "premiere":
+			return varPrixUnitaire * 2; 
+			
+		case "buisness": 
+			return varPrixUnitaire * 2 + 300;
+			
+		default:
+			return varPrixUnitaire;
+		}
+		
+	}
+	public CalculPrixUnitaire getCalculPrixUnitaire() {
+		return calculPrixUnitaire;
+	}
+	public void setCalculPrixUnitaire(CalculPrixUnitaire calculPrixUnitaire) {
+		this.calculPrixUnitaire = calculPrixUnitaire;
+	}
+	
+}
---
+++ src/main/java/fr/eftl/agence/voyages/prix/CalculPrixUnitaire.java
@@ -0,0 +1,37 @@
+package fr.eftl.agence.voyages.prix;
+
+public class CalculPrixUnitaire {
+
+	private String depart; 
+	private String arrivee; 
+	
+	
+	public CalculPrixUnitaire(String depart, String arrivee) {
+		this.setDepart(depart); 
+		this.setArrivee(arrivee);
+	}
+	
+	public CalculPrixUnitaire() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Integer generer() {
+		return null;
+	}
+
+	public String getDepart() {
+		return depart;
+	}
+
+	public void setDepart(String depart) {
+		this.depart = depart;
+	}
+
+	public String getArrivee() {
+		return arrivee;
+	}
+
+	public void setArrivee(String arrivee) {
+		this.arrivee = arrivee;
+	}
+}
